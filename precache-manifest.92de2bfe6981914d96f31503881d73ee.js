11aca92e2e35522943112d452b92768efddc8c97
BagchiMB <mbagchi6884@gmail.com>
2020-04-08 05:18:25 +0530

    Updates

---
+++ precache-manifest.92de2bfe6981914d96f31503881d73ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2472c9bfd792f72398b8ad124674bb07",
+    "url": "/DP/index.html"
+  },
+  {
+    "revision": "3255bd7dfbea158752f3",
+    "url": "/DP/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "136d755fe10c3a4c92ba",
+    "url": "/DP/static/css/main.4ebbcb31.chunk.css"
+  },
+  {
+    "revision": "3255bd7dfbea158752f3",
+    "url": "/DP/static/js/2.365be350.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/DP/static/js/2.365be350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136d755fe10c3a4c92ba",
+    "url": "/DP/static/js/main.75afd3aa.chunk.js"
+  },
+  {
+    "revision": "508d754292a5d86f894f",
+    "url": "/DP/static/js/runtime-main.43cfeb45.js"
+  },
+  {
+    "revision": "e69ebdcb606f36b1a9292a7902dfaf38",
+    "url": "/DP/static/media/bg3.e69ebdcb.svg"
+  }
+]);
